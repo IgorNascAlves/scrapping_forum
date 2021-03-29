89558cdf86be2f272d6c09cf8211f741e81a5d4d
IgorNascAlves <igor.nascimento.flipe@gmail.com>
2021-03-28 21:45:37 -0300

    refatoração do código

---
+++ README.md
@@ -0,0 +1,11 @@
+Desafio de Web Scraping :warning: 
+
+Objetivo: fazer um scraping que percorra todas as subcategorias do fórum e retorne o número de tópicos sem respostas de cada uma delas. Imagem demonstrando as subcategorias do fórum (https://i.imgur.com/OU1AK2c.png)
+
+Código base usando selenium. Esse código funciona, mas não é a melhor abordagem: https://gist.github.com/NadiaaOliverr/e9ad7618a73ac5287ecdfabcea0a2a45
+
+Desafio proposto: elaboração de um código que preze por boas práticas e que cumpra com o objetivo. 
+
+API da alura que retorna o slug de url das subcategorias (estude essa API e trabalhe com esses dados):  https://cursos.alura.com.br/api/categorias
+
+Prazo: Um mês, prazo final dia 22/04
--- main.py
+++ main.py
@@ -1,79 +1,35 @@
-# inicio do tempo
-from datetime import datetime
-start_time = datetime.now()
+import pandas as pd
+import re
+import requests
 
 
 # leitura das urls API
 
-
-import pandas as pd
-
-url = 'https://cursos.alura.com.br/api/categorias'
-df = pd.read_json(url, orient='records')
-
-inicio = 'https://cursos.alura.com.br/forum/subcategoria-'
-fim = '/sem-resposta/1'
-lista_link_nome = [((inicio + subcategoria['slug'] + fim), subcategoria['nome']) for categoria in df['subcategorias'] for subcategoria in categoria]
-
-df = pd.DataFrame(lista_link_nome)
-df.to_csv('teste.csv', header=False, index=False)
-
-# leitura das urls selenium
-
-# from selenium import webdriver
-# import pandas as pd
-
-# driver = webdriver.Firefox()
-
-# url_base = "https://cursos.alura.com.br/forum/todos/1"
-
-# driver.get(url_base)
-
-# categorias = driver.find_elements_by_class_name("dashboard-list-subcategory-link")
-
-# categorias = [(categoria.get_attribute("href"),categoria.get_attribute("text").split()[0]) for categoria in categorias]
-
-# driver.quit()
-
-# df = pd.DataFrame(categorias)
-# df.to_csv('teste.csv', header=False, index=False)
+url: str = 'https://cursos.alura.com.br/api/categorias'
+df_json: pd.DataFrame = pd.read_json(url, orient='records')
+inicio: str = 'https://cursos.alura.com.br/forum/subcategoria-'
+fim: str = '/sem-resposta/1'
+lista_link_nome = [((inicio + subcategoria['slug'] + fim), subcategoria['nome']) for categoria in df_json['subcategorias'] for subcategoria in categoria]
+df: pd.DataFrame = pd.DataFrame(lista_link_nome,columns=["URL", 'Nome'])
 
 #calculo da quantidade de duvidas
 
-df = pd.read_csv("teste.csv",names=["URL", 'nome'])
-#df['url'] = [url[:-5] + 'sem-resposta/1' for url in df['URL']] #selenium
-
-import re
-import requests
-
 lista = []
+sessao = requests.Session()
+regex_paginas = 'paginationLink'
+regex_topicos = 'class="forumList-item-subject"'
+
 for url in df['URL']:
-    sessao = requests.Session()
-    resposta = sessao.get(url)
-    regex = 'paginationLink'
-    count = len(re.findall(regex, str(resposta.content)))
 
     resposta = sessao.get(url)
-    regex = 'class="forumList-item-subject"'
-    count_page = len(re.findall(regex, str(resposta.content)))
-
-    resposta = sessao.get(url[:-1] + str(count))
-    regex = 'class="forumList-item-subject"'
-    count_last_pag = len(re.findall(regex, str(resposta.content)))
-
-    total_sub = count_last_pag + ((count - 1) * count_page) if count else count_last_pag
+    resp_content_str = str(resposta.content)
+    qtd_pag = len(re.findall(regex_paginas, resp_content_str))        
+    qtd_topicos = len(re.findall(regex_topicos, resp_content_str))
+    
+    resposta = sessao.get(url[:-1] + str(qtd_pag))    
+    qtd_topicos_ultima_pag = len(re.findall(regex_topicos, str(resposta.content)))    
+    total_sub = qtd_topicos_ultima_pag + ((qtd_pag - 1) * qtd_topicos) if qtd_pag else qtd_topicos_ultima_pag    
     lista.append(total_sub)
 
-df['count'] = lista
-
-df.to_csv('teste2.csv', index=False)
-
-#consulta
-
-df = pd.read_csv("teste2.csv")
-print(df.iloc[110][-2], df.iloc[110][-1], sep='\n')
-
-#fim do tempo
-
-end_time = datetime.now()
-print('Duração: {}'.format(end_time - start_time))
+df['Quantidade'] = lista
+df.to_csv('subcategoria_topicos_sem_resposta.csv', index=False)
