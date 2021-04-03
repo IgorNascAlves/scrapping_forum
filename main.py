876ebcc7529108371204dd2378ca962820a5c6c7
IgorNascAlves <igor.nascimento.flipe@gmail.com>
2021-04-02 21:09:28 -0300

    experimentando diferentes abordagens

--- main.py
+++ main.py
@@ -1,35 +1,48 @@
-import pandas as pd
-import re
 import requests
+import pandas as pd
+from datetime import datetime
+from bs4 import BeautifulSoup
 
 
 # leitura das urls API
+def pega_URLs_Nomes_salva_arquivo():
+    url: str = 'https://cursos.alura.com.br/api/categorias'
+    df_json: pd.DataFrame = pd.read_json(url, orient='records')
 
-url: str = 'https://cursos.alura.com.br/api/categorias'
-df_json: pd.DataFrame = pd.read_json(url, orient='records')
-inicio: str = 'https://cursos.alura.com.br/forum/subcategoria-'
-fim: str = '/sem-resposta/1'
-lista_link_nome = [((inicio + subcategoria['slug'] + fim), subcategoria['nome']) for categoria in df_json['subcategorias'] for subcategoria in categoria]
-df: pd.DataFrame = pd.DataFrame(lista_link_nome,columns=["URL", 'Nome'])
+    inicio: str = 'https://cursos.alura.com.br/forum/subcategoria-'
+    fim: str = '/sem-resposta/1'
+    
+    lista_link_nome = [((inicio + subcategoria['slug'] + fim), subcategoria['nome']) for categoria in df_json['subcategorias'] for subcategoria in categoria]
+    
+    df: pd.DataFrame = pd.DataFrame(lista_link_nome,columns=["URL", 'Nome'])
+    df.to_csv('urls_subcategorias.csv', index=False)
 
 #calculo da quantidade de duvidas
+def pega_qtd_topicos_salva_arquivo():
 
-lista = []
-sessao = requests.Session()
-regex_paginas = 'paginationLink'
-regex_topicos = 'class="forumList-item-subject"'
+    df = pd.read_csv('urls_subcategorias.csv')
 
-for url in df['URL']:
+    lista = []
+    qtd_topicos = 20
 
-    resposta = sessao.get(url)
-    resp_content_str = str(resposta.content)
-    qtd_pag = len(re.findall(regex_paginas, resp_content_str))        
-    qtd_topicos = len(re.findall(regex_topicos, resp_content_str))
-    
-    resposta = sessao.get(url[:-1] + str(qtd_pag))    
-    qtd_topicos_ultima_pag = len(re.findall(regex_topicos, str(resposta.content)))    
-    total_sub = qtd_topicos_ultima_pag + ((qtd_pag - 1) * qtd_topicos) if qtd_pag else qtd_topicos_ultima_pag    
-    lista.append(total_sub)
+    for url in df['URL']:
 
-df['Quantidade'] = lista
-df.to_csv('subcategoria_topicos_sem_resposta.csv', index=False)
+        page = requests.get(url)
+        soup = BeautifulSoup(page.text, 'html.parser')        
+        qtd_pag = len(soup.find_all(class_='paginationLink'))
+        
+        page = requests.get(url[:-1] + str(qtd_pag))    
+        soup = BeautifulSoup(page.text, 'html.parser')        
+        qtd_topicos_ultima_pag =  len(soup.find_all(class_='forumList-item-subject'))
+        
+        total_sub = qtd_topicos_ultima_pag + ((qtd_pag - 1) * qtd_topicos) if qtd_pag > 1 else qtd_topicos_ultima_pag    
+        lista.append(total_sub)
+
+    df['Quantidade'] = lista
+    df.to_csv('subcategoria_topicos_sem_resposta.csv', index=False)
+
+if __name__ == '__main__':
+    start_time = datetime.now()
+    pega_qtd_topicos_salva_arquivo()
+    end_time = datetime.now()
+    print('Duração: {}'.format(end_time - start_time))
